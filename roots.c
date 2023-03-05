b5cad5f0bcc1a353426d053eec6977bacef1e108
yeshwanthsai123 <113962518+yeshwanthsai123@users.noreply.github.com>
2023-03-05 21:33:19 +0530

    Create roots.c

---
+++ roots.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int root1(int a,int b,int c);
+int root2(int a,int b,int c);
+int main(){
+	int a,b,c;
+	printf("enter the values of a,b,c");
+	scanf("%d,%d,%d",&a,&b,&c);
+	root1(a,b,c);
+	root2(a,b,c);
+	return 0;
+}
+int root1(int a,int b,int c)
+{
+	int s;
+	int x=(-b+s)/2*a;
+	s=sqrt(b*b-(4*a*c));
+	printf("the root is %d\n",x);
+	return x;
+}
+int root2(int a,int b,int c)
+{
+    int s;
+    int y=(-b+s)/2*a;
+    s=sqrt(b*b-(4*a*c));
+
+    printf("the root is %d",y);
+    return y;
+}
