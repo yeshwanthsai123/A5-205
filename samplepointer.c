2da192758fedfbbd3d32832d66d021d09490891f
yeshwanthsai123 <113962518+yeshwanthsai123@users.noreply.github.com>
2023-04-06 15:58:58 +0530

    Create samplepointer.c

---
+++ samplepointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int addr;
+	int addr2;
+	int addr3;
+	printf("the address of addr is %u\n",&addr);//gives address or location where this addr ,addr3,addr3 are stored//
+    printf("the address of addr2 is %u\n",&addr2);
+    printf("the address of addr3 is %u",&addr3);//
+}
