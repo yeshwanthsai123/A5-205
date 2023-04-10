8ec4aa2804061f7014209f1993367b427cc71269
yeshwanthsai123 <113962518+yeshwanthsai123@users.noreply.github.com>
2023-04-10 18:51:16 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int Fibonacci(int);
+int main()
+{
+int n, i = 0, c;
+printf("Enter a number ");
+scanf("%d",&n);
+printf("Fibonacci series\n");
+for ( c = 1 ; c <= n ; c++ )
+{
+printf("%d\n", Fibonacci(i));
+i++;
+}
+Fibonacci(n);
+}
+int Fibonacci(int n)
+{
+if ( n == 0 )
+return 0;
+else if ( n == 1 )
+return 1;
+else
+return ( Fibonacci(n-1) + Fibonacci(n-2) );
+}
